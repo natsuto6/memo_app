2acb382f2ef226a4f626e0b210be6121c73e1986
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2022-12-29 16:59:33 +0900

    メモアプリ初回コミット

---
+++ memo.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+FILE_PATH = 'public/memos.json'
+
+def get_memos(file_path)
+  File.open(file_path) { |f| JSON.parse(f.read) }
+end
+
+def set_memos(file_path, memos)
+  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
+end
+
+get '/' do
+  redirect '/memos'
+end
+
+get '/memos' do
+  @memos = get_memos(FILE_PATH)
+  erb :index
+end
+
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos' do
+  @title = params[:title]
+  @content = params[:content]
+
+  memos = get_memos(FILE_PATH)
+  id = (memos.keys.map(&:to_i).max + 1).to_s
+  memos[id] = { 'title' => @title, 'content' => @content }
+  set_memos(FILE_PATH, memos)
+
+  redirect '/memos'
+end
+
+get '/memos/:id' do
+  @memos = get_memos(FILE_PATH)
+  @title = @memos[params[:id]]['title']
+  @content = @memos[params[:id]]['content']
+  erb :show
+end
+
+delete '/memos/:id' do
+  memos = get_memos(FILE_PATH)
+  memos.delete(params[:id])
+  set_memos(FILE_PATH, memos)
+
+  redirect '/memos'
+end
+
+get '/memos/:id/edit' do
+  memos = get_memos(FILE_PATH)
+  @title = memos[params[:id]]['title']
+  @content = memos[params[:id]]['content']
+  erb :edit
+end
+
+patch '/memos/:id' do
+  @title = params[:title]
+  @content = params[:content]
+
+  memos = get_memos(FILE_PATH)
+  memos[params[:id]] = { 'title' => @title, 'content' => @content }
+  set_memos(FILE_PATH, memos)
+
+  redirect "/memos/#{params[:id]}"
+end
