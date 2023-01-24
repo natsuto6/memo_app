22fbc3f8e244b47596580b376d106067646045bc
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-01-24 22:46:17 +0900

    指定したメモの表示、メモの削除

--- memo.rb
+++ memo.rb
@@ -23,8 +23,17 @@ def load_memos
   conn.exec('SELECT * FROM memos')
 end
 
+def load_memo(id)
+  result = conn.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
+  result.tuple(0).values
+end
+
 def memo_creation(title, content)
   conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
+end
+
+def memo_deletion(id)
+  conn.exec_params('DELETE FROM memos WHERE id = $1;', [id])
 end
 
 get '/' do
@@ -44,22 +53,18 @@ post '/memos' do
   title = params[:title]
   content = params[:content]
   memo_creation(title, content)
-  
   redirect '/memos'
 end
 
 get '/memos/:id' do
-  @memos = get_memos(FILE_PATH)
-  @title = @memos[params[:id]]['title']
-  @content = @memos[params[:id]]['content']
+  memo = load_memo(params[:id])
+  @title = memo[1]
+  @content = memo[2]
   erb :show
 end
 
 delete '/memos/:id' do
-  memos = get_memos(FILE_PATH)
-  memos.delete(params[:id])
-  set_memos(FILE_PATH, memos)
-
+  memo_deletion(params[:id])
   redirect '/memos'
 end
 
